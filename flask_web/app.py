f54fc0fef50e2bcfc1b8cd08716b249d64933ea8
Thomas McCall <thomas.e.mccall@gmail.com>
2019-06-26 14:29:05 -0500

    simple hello world-type flask app

--- flask_web/app.py
+++ flask_web/app.py
@@ -1,10 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'flask in a docker container!!'
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/")
+def hello():
+    return "Hello World!"
